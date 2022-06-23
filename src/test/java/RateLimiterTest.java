7d86ebbc2fc6d995b7aadde06a340212e3903a3e
CHITTA RANJAN SWAIN <chittaranjan138@gmail.com>
2022-06-23 14:43:13 +0530

    Added Test for RateLimited Function

---
+++ src/test/java/RateLimiterTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+public class RateLimiterTest {
+    @Test
+    public void testRateLimitedFunctionWorksWithInitialTokens() throws Exception {
+        RateLimiter rateLimiter = new RateLimiterImplWithTokenBucket(3, 3, 0, 1);
+        Function<Integer, Integer> square = x -> x * x;
+        Function<Integer, Integer> rateLimited = rateLimiter.wrap(square);
+        IntStream.range(0, 3).parallel().forEach(
+                x -> System.out.println(rateLimited.apply(4)));
+
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void testRateLimitedFunctionFailsWithMoreThanInitialTokens() {
+        RateLimiter rateLimiter = new RateLimiterImplWithTokenBucket(4, 4, 0, 1);
+        Function<Integer, Integer> square = x -> x * x;
+        Function<Integer, Integer> rateLimited = rateLimiter.wrap(square);
+        IntStream.range(0, 5).parallel().forEach(
+                x -> System.out.println(rateLimited.apply(4)));
+    }
+
+    @Test
+    public void testRateLimitedFunctionWorksUponRetryingAfterWindowSize() throws InterruptedException {
+        RateLimiter rateLimiter = new RateLimiterImplWithTokenBucket(4, 4, 4, 1);
+        Function<Integer, Integer> square = x -> x * x;
+        Function<Integer, Integer> rateLimited = rateLimiter.wrap(square);
+        try {
+            IntStream.range(0, 5).parallel().forEach(
+                    x -> System.out.println(rateLimited.apply(4)));
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
+        }
+        Thread.sleep(1000);
+        System.out.println(rateLimited.apply(4));
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void testRateLimitedFunctionWorksUponRetryingBeforeWindowSize() throws InterruptedException {
+        RateLimiter rateLimiter = new RateLimiterImplWithTokenBucket(4, 4, 4, 2);
+        Function<Integer, Integer> square = x -> x * x;
+        Function<Integer, Integer> rateLimited = rateLimiter.wrap(square);
+        try {
+            IntStream.range(0, 5).parallel().forEach(
+                    x -> System.out.println(rateLimited.apply(4)));
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
+        }
+        Thread.sleep(1000);
+        System.out.println(rateLimited.apply(4));
+    }
+}
